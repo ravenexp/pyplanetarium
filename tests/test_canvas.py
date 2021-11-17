bf5fe375b1bdff15e2b1df9aaf1d04afcdd4b355
Sergey Kvachonok <ravenexp@gmail.com>
2021-11-17 11:33:30 +0300

    test: Add spot parameters adjustment unit test
    Split the spot state mutation checks into a new test.
    Invoke the spot parameters query methods to verify the updated state.
    
    Add tests for the RAW image format export feature.

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -59,13 +59,52 @@ class CanvasCase(unittest.TestCase):
 
         canvas.draw()
 
+    def test_move_spots(self) -> None:
+        """
+        Light spots moving test
+        """
+
+        shape1 = SpotShape().scale(3.5)
+        shape2 = SpotShape().scale(5.5)
+
+        width = 1024
+        height = 768
+
+        canvas = Canvas.new(width, height)
+        spot1 = canvas.add_spot((100.5, 200.7), shape1, 0.8)
+        spot2 = canvas.add_spot((400.5, 600.7), shape2, 0.6)
+
+        pos1 = canvas.spot_position(spot1)
+        pos2 = canvas.spot_position(spot2)
+        assert pos1 is not None
+        assert pos2 is not None
+        self.assertAlmostEqual(pos1[0], 100.5, 4)
+        self.assertAlmostEqual(pos2[1], 600.7, 4)
+
+        int1 = canvas.spot_intensity(spot1)
+        int2 = canvas.spot_intensity(spot2)
+        assert int1 is not None
+        assert int2 is not None
+        self.assertAlmostEqual(int1, 0.8, 4)
+        self.assertAlmostEqual(int2, 0.6, 4)
+
         canvas.set_spot_offset(spot2, (5.5, -7.0))
         canvas.set_spot_illumination(spot2, 0.5)
 
         canvas.set_background(6000)
         canvas.set_brightness(1.3)
 
-        self.assertNotEqual(spot1, spot2)
+        pos2 = canvas.spot_position(spot2)
+        assert pos2 is not None
+        self.assertAlmostEqual(pos2[0], 400.5 + 5.5, 4)
+        self.assertAlmostEqual(pos2[1], 600.7 - 7.0, 4)
+
+        int1 = canvas.spot_intensity(spot1)
+        int2 = canvas.spot_intensity(spot2)
+        assert int1 is not None
+        assert int2 is not None
+        self.assertAlmostEqual(int1, 0.8 * 1.3, 4)
+        self.assertAlmostEqual(int2, 0.6 * 0.5 * 1.3, 4)
 
         canvas.draw()
 
@@ -91,6 +130,24 @@ class CanvasCase(unittest.TestCase):
 
         canvas.draw()
 
+        raw8_bytes = canvas.export_image(ImageFormat.RawGamma8Bpp)
+        self.assertIsInstance(raw8_bytes, bytes)
+        self.assertEqual(len(raw8_bytes), 65536)
+        self.assertEqual(raw8_bytes[0], 78)
+        self.assertEqual(raw8_bytes[65535], 78)
+
+        raw10_bytes = canvas.export_image(ImageFormat.RawLinear10BppLE)
+        self.assertIsInstance(raw10_bytes, bytes)
+        self.assertEqual(len(raw10_bytes), 2 * 65536)
+        self.assertEqual(raw10_bytes[0], 78)
+        self.assertEqual(raw10_bytes[1], 0)
+
+        raw12_bytes = canvas.export_image(ImageFormat.RawLinear12BppLE)
+        self.assertIsInstance(raw12_bytes, bytes)
+        self.assertEqual(len(raw12_bytes), 2 * 65536)
+        self.assertEqual(raw12_bytes[0], 56)
+        self.assertEqual(raw12_bytes[1], 1)
+
         png8_bytes = canvas.export_image(ImageFormat.PngGamma8Bpp)
         self.assertIsInstance(png8_bytes, bytes)
         self.assertEqual(len(png8_bytes), 949)
@@ -98,6 +155,15 @@ class CanvasCase(unittest.TestCase):
         png16_bytes = canvas.export_image(ImageFormat.PngLinear16Bpp)
         self.assertIsInstance(png16_bytes, bytes)
         self.assertEqual(len(png16_bytes), 1816)
+
+        # with open("image8.raw", "wb") as f:
+        #     f.write(raw8_bytes)
+
+        # with open("image10.raw", "wb") as f:
+        #     f.write(raw10_bytes)
+
+        # with open("image12.raw", "wb") as f:
+        #     f.write(raw12_bytes)
 
         # with open("image8.png", "wb") as f:
         #     f.write(png8_bytes)
